810f6d226765e97f5d4f3adb62196e63ba80f2dc
Andrii-Posternak <panposternak@gmail.com>
2023-04-18 07:35:43 +0300

    delete comments

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,4 @@
 const RequestError = require("./RequestError");
-const sendEmail = require("./sendEmail");
 const calcDailyCalorieNorm = require("./calc");
 
-module.exports = { RequestError, sendEmail, calcDailyCalorieNorm };
+module.exports = { RequestError, calcDailyCalorieNorm };
